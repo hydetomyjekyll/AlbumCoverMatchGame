769a98974e1985884321f56167bf009e4da011af
Birendra Jha <birendra.jha1973@outlook.com>
2018-06-26 08:52:27 +0530

    Made the logic for accessing library

--- AlbumCoverMatchGame/MainPage.xaml.cs
+++ AlbumCoverMatchGame/MainPage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,5 +30,86 @@ namespace AlbumCoverMatchGame
         {
             this.InitializeComponent();
         }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //1. Get access to music library
+
+            //Get the users music library
+            StorageFolder folder = KnownFolders.MusicLibrary;
+
+            //Make a observable collection of StorageFile type to make store all the songs
+            var allSongs = new ObservableCollection<StorageFile>();
+
+            //Call the function to get all songs in music library
+            await RetrieveFilesAndFolder(allSongs, folder);
+
+            //Pick 10 random Songs
+            var randomSongs = await PickRandomSongs(allSongs);
+
+
+
+        }
+
+        private async Task RetrieveFilesAndFolder(ObservableCollection<StorageFile> list, StorageFolder parent)
+        {
+            //For every files in the parent add all mp3 to observable coleection
+            foreach (var item in await parent.GetFilesAsync())
+            {
+                if (item.FileType == ".mp3")
+                    list.Add(item);
+            }
+
+            //for each folder recursively call itself
+            foreach (var item in await parent.GetFoldersAsync())
+            {
+                await RetrieveFilesAndFolder(list, item);
+            }   
+        }
+
+
+        private async Task<List<StorageFile>> PickRandomSongs(ObservableCollection<StorageFile> allSongs)
+        {
+            Random random = new Random();
+
+            var totalSongsCount = allSongs.Count;
+
+            var randomSongs = new List<StorageFile>();
+
+            while (randomSongs.Count < 10)
+            {
+                var randomNumber = random.Next(totalSongsCount);
+                var randomSong = allSongs[randomNumber];
+
+                MusicProperties randomSongMusicProperties = 
+                    await randomSong.Properties.GetMusicPropertiesAsync();
+
+                bool isDuplicate = false;
+
+                if (String.IsNullOrEmpty(randomSongMusicProperties.Album))
+                {
+                    isDuplicate = true;
+                }
+                else
+                {
+                    foreach (var song in randomSongs)
+                    {
+                        MusicProperties songMusicProperties = await song.Properties.GetMusicPropertiesAsync();
+
+                        if(randomSongMusicProperties.Album == songMusicProperties.Album)
+                        {
+                            isDuplicate = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!isDuplicate)
+                    randomSongs.Add(randomSong);
+            }
+
+            return randomSongs;
+        }
+
     }
 }
